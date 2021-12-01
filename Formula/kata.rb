4a6d82796849ed9428b3bd8d4853de480fc3d37f
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-01 14:40:48 +0100

    Brew formula update for kata version v1.1.0

--- Formula/kata.rb
+++ Formula/kata.rb
@@ -5,29 +5,52 @@
 class Kata < Formula
   desc "Kata 形🤺 exercises"
   homepage "https://github.com/arvenil/kata"
-  version "1.0.1"
-  bottle :unneeded
+  version "1.1.0"
 
-  if OS.mac?
-    url "https://github.com/arvenil/kata/releases/download/v1.0.1/kata_1.0.1_Darwin_x86_64.tar.gz"
-    sha256 "44e9cb6c9842733c92a4b7e6dcd95042e92d1700b7ae16175e37930adb64d840"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/arvenil/kata/releases/download/v1.0.1/kata_1.0.1_Linux_x86_64.tar.gz"
-    sha256 "a5a1d84c36534676b348df6be26ea4ea3ead611bdd7a4a8d1f2000db71b36def"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/arvenil/kata/releases/download/v1.0.1/kata_1.0.1_Linux_arm64.tar.gz"
-    sha256 "b45fee78c1bf2930fbc2c4f44966de782b0e5eb97ca0d6c6e8aa0c7f8e18e5d5"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/arvenil/kata/releases/download/v1.1.0/kata_1.1.0_Darwin_arm64.tar.gz"
+      sha256 "fc7128d4b20b8bee5cb2f0cd5f5cf5d8c48801760a9c0b3b48db453e6d1c743d"
+
+      def install
+        bin.install "bsearch"
+        bin.install "ladder"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/arvenil/kata/releases/download/v1.1.0/kata_1.1.0_Darwin_x86_64.tar.gz"
+      sha256 "ce73662f71b45fb5474de424edf7b1434a8fc50f7612a4df337c43305b63f537"
+
+      def install
+        bin.install "bsearch"
+        bin.install "ladder"
+      end
+    end
   end
 
-  def install
-    bin.install "kata02"
-    bin.install "kata19"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/arvenil/kata/releases/download/v1.1.0/kata_1.1.0_Linux_arm64.tar.gz"
+      sha256 "89eee3c329db6ca57d22057a0a09f9af0e7894596e981896f46ed08ec4675e05"
+
+      def install
+        bin.install "bsearch"
+        bin.install "ladder"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/arvenil/kata/releases/download/v1.1.0/kata_1.1.0_Linux_x86_64.tar.gz"
+      sha256 "3af1119cade2bf1d7d0efd35df2f45196a502961a2d70f630cdf42d6e2a6eff0"
+
+      def install
+        bin.install "bsearch"
+        bin.install "ladder"
+      end
+    end
   end
 
   test do
-    system "#{bin}/kata02 -h"
-    system "#{bin}/kata19 -h"
+    system "#{bin}/bsearch -h"
+    system "#{bin}/ladder -h"
   end
 end
